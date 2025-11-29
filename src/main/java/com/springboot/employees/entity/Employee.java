432bcc659ec529e1deac081085dd0eb94cb67e5b
mustafa-mohamedz <cse.mustafa.mohamed@gmail.com>
2025-11-29 16:00:16 +0200

    created Employee entity

---
+++ src/main/java/com/springboot/employees/entity/Employee.java
@@ -0,0 +1,60 @@
+package com.springboot.employees.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    @Column
+    private String email;
+
+
+    public Employee (String firstName, Long id, String lastName, String email) {
+        this.firstName = firstName;
+        this.id = id;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
